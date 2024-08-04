e503fc8c103cb4796ac34c51377f13ab5872e6a2
Nassam123 <130648214+Nassam123@users.noreply.github.com>
2024-08-04 13:23:00 +0300

    Task44

---
+++ Backend Tasks/Controllers/Task44.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Tasks.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class Greeter : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string name = "Anonymous")
+        {
+            return Ok($"Hello {name}");
+        }
+    }
+}
